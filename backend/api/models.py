f7f41590f4420b495de80d9dd5ff5f817ffb66b2
Joao Vitor <jvrezendemoura@gmail.com>
2025-08-19 06:40:47 -0300

    feature(api): Creating the api app in the django archtecture

---
+++ backend/api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    pass
+
+class Student(models.Model):
+    pass
+
+class Class(models.Model):
+    pass
+
+class Subject(models.Model):
+    pass
+
+class Teacher(models.Model):
+    pass
