1cf3481ea6de3f94dae35266e2538fd6e4a7a87c
Vitor Moura <jvrezendemoura@gmail.com>
2025-08-19 17:42:12 -0300

    feature(user-model): Creating the User get and post method

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render 
 from django.urls import path
+from api import views
+from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
 
 urlpatterns = [
-    
-
-
+    path("users/", views.UserView.as_view())
 ]
 
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from rest_framework.views import APIView 
+from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth.models import User, Group 
+from api.serializers import UserSerializer
 
 class UserView(APIView):
     """
-    
+    View for the user object inside of the api
     """
     def get(self, request) -> None:
         """
         Return a list with the classes in the database
         """
-        pass
+        users = User.objects.all()
+        serialized_data = UserSerializer(users, many=True)
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
 
     def post(self, request) -> None:
         """
         Create a new element inside of the group
         """
-        pass
+        serialized_data = UserSerializer(data=request.data)
+        print(serialized_data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
